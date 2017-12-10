8d3f9b6c1bb92e1fdfa9a9648d7ed0512e85daee
Jerry W <jerrywiltse@gmail.com>
2017-12-10 16:15:58 -0500

    real close

--- src/libhandler_example.c
+++ src/libhandler_example.c
@@ -5,7 +5,7 @@ LH_DEFINE_EFFECT1(exn, raise)
 LH_DEFINE_VOIDOP1(exn, raise, lh_string)
 
 lh_value handle_exn_raise(lh_resume* r, lh_value local, lh_value arg) {
-  printf("exception raised: %s\n", string_value(arg));
+  printf("exception raised: %s\n", lh_lh_string_value(arg));
   return lh_value_null;
 }
 
@@ -28,11 +28,12 @@ lh_value my_exn_handle(lh_value(*action)(value), lh_value arg)
 
 lh_value divide_by(lh_value x)
 {
-  return value_long(divexn(42, long_value(x)));
+  return lh_value_long(divexn(42, lh_long_value(x)));
 }
 
 int main()
 {
-  my_exn_handle(divide_by, value_long(0));
+  my_exn_handle(divide_by, lh_value_long(0));
+  char g = getchar();
   return 0;
 }
